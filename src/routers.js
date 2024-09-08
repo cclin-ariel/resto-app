a87426075dd5154a5edf01cc44cbf368cd5dff91
cclin <71698978+cclin-ariel@users.noreply.github.com>
2024-09-08 10:07:37 +0900

    reset

--- src/routers.js
+++ src/routers.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./components/Home.vue";
 import SignUp from "./components/SignUp.vue";
 import Login from "./components/Login.vue";
@@ -34,7 +34,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
